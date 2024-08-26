acf27e5e46728fea682087ff81b56707a4230e55
Anas Karatela <anas.karatela@gmail.com>
2024-08-26 14:33:47 +0530

    2nd

--- src/v1/routes.v1.js
+++ src/v1/routes.v1.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const statusRoutes = require('./status/status.routes');
 const signupRoutes = require('./signup/signup.routes')
 const loginRoutes = require('./login/login.routes');
 
 const router = express.Router();
 
-router.use('/status', statusRoutes);
 router.use('/signup', signupRoutes);
 router.use('/login', loginRoutes);
 
